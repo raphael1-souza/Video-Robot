915fa8f28ac78af140321cfb80aef5a4833bb4d1
Raphael <raphasilvasouza96@gmail.com>
2019-03-25 09:21:29 -0300

    [FEAT] Add google Trends

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const readline = require ('readline-sync')
+const googleTrends = require('google-trends-api');
 
-function start(){
+async function start(){
     const content = {}
     content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
+    // content.test= test(content.searchTerm)
+    content.prefix = askAndReturnPrefix(content.searchTerm)
+    console.log(content);
     function askAndReturnSearchTerm(){
-        return readline.question('Type a Wikepedia search term: ')
+       const response = readline.question('Type a Wikepedia search term: ')
+       return response
+
     }
     function askAndReturnPrefix(){
         const prefixes = ['Who is', 'What is', 'The history of']
         const selectedPrefixIndex = readline.keyInSelect(prefixes,'Choose one option: ')
         const selectedPrefixText = prefixes[selectedPrefixIndex]
-        
         return selectedPrefixText
     }
-    console.log(content)
+
+
+/////////////////Projeto futuro
+    // function test(palavra){
+    //     googleTrends.relatedQueries({keyword: palavra})
+    //     .then((res) => {
+    //      obj = JSON.parse(res);
+    //      resJson = JSON.parse(JSON.stringify(obj.default.rankedList[0]))
+    //      var count = Object.keys(obj.default.rankedList[0].rankedKeyword).length; // contador de quantos itens há no objeto
+    //      vetorRelacional = new Array(count);
+    //      for(i=0;i<9;i++){
+    //         vetorRelacional[i] =resJson.rankedKeyword[i].query
+ 
+    //      }
+    //      const selectedPrefixIndex = readline.keyInSelect(vetorRelacional,'Choose one option: ')
+    //     //  const v = vetorRelacional[0]
+    //     //  return v;
+         
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     })
+    //     }
 }
+
+
+
 
 start()
