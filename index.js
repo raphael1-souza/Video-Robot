4a5fd362752fbd41a1dcbc996928d7c338800218
Raphael <raphasilvasouza96@gmail.com>
2019-04-01 17:29:30 -0300

    [FEAT] add input robot and state robot

--- index.js
+++ index.js
@@ -1,54 +1,16 @@
-const readline = require ('readline-sync')
-const googleTrends = require('google-trends-api');
+
+
 const robots = {
-    text: require('./robots/text.js')
+    text: require('./robots/text.js'),
+    input:require('./robots/input.js'),
+    state: require ('./robots/state.js')
 }
 async function start(){
-    const content = {
-        maximumSentences:7
-    }
-    content.searchTerm = askAndReturnSearchTerm()
-    // content.test= test(content.searchTerm)
-    content.prefix = askAndReturnPrefix(content.searchTerm)
-    await robots.text(content)
 
-    // console.log(content);
-    function askAndReturnSearchTerm(){
-       const response = readline.question('Type a Wikepedia search term: ')
-       return response
-
-    }
-    function askAndReturnPrefix(){
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes,'Choose one option: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        return selectedPrefixText
-    }
-
-    console.log(JSON.stringify(content,null,4));
-
-
-/////////////////Projeto futuro
-    // function test(palavra){
-    //     googleTrends.relatedQueries({keyword: palavra})
-    //     .then((res) => {
-    //      obj = JSON.parse(res);
-    //      resJson = JSON.parse(JSON.stringify(obj.default.rankedList[0]))
-    //      var count = Object.keys(obj.default.rankedList[0].rankedKeyword).length; // contador de quantos itens há no objeto
-    //      vetorRelacional = new Array(count);
-    //      for(i=0;i<9;i++){
-    //         vetorRelacional[i] =resJson.rankedKeyword[i].query
- 
-    //      }
-    //      const selectedPrefixIndex = readline.keyInSelect(vetorRelacional,'Choose one option: ')
-    //     //  const v = vetorRelacional[0]
-    //     //  return v;
-         
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     })
-    //     }
+    robots.input()
+    await robots.text()
+    const content = robots.state.load()
+    console.dir(content,{depth:null})
 }
 
 
---
+++ robots/input.js
@@ -0,0 +1,50 @@
+const readline = require ('readline-sync')
+const googleTrends = require('google-trends-api');
+const state = require('./state.js')
+
+function robot(){
+    const content = {
+        maximumSentences:7
+    }
+
+    content.searchTerm = askAndReturnSearchTerm()
+    // content.test= test(content.searchTerm)
+    content.prefix = askAndReturnPrefix(content.searchTerm)
+    state.save(content)
+
+    function askAndReturnSearchTerm(){
+        const response = readline.question('Type a Wikepedia search term: ')
+        return response
+
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes,'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+        return selectedPrefixText
+    }
+    /////////////////Projeto futuro
+    // function test(palavra){
+    //     googleTrends.relatedQueries({keyword: palavra})
+    //     .then((res) => {
+    //      obj = JSON.parse(res);
+    //      resJson = JSON.parse(JSON.stringify(obj.default.rankedList[0]))
+    //      var count = Object.keys(obj.default.rankedList[0].rankedKeyword).length; // contador de quantos itens há no objeto
+    //      vetorRelacional = new Array(count);
+    //      for(i=0;i<9;i++){
+    //         vetorRelacional[i] =resJson.rankedKeyword[i].query
+ 
+    //      }
+    //      const selectedPrefixIndex = readline.keyInSelect(vetorRelacional,'Choose one option: ')
+    //     //  const v = vetorRelacional[0]
+    //     //  return v;
+         
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     })
+    //     }
+}
+
+module.exports = robot
